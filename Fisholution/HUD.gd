833a8b70867c729484eac59ef6da747748994507
nordicebear <nordicebeargames@gmail.com>
2019-03-27 23:35:21 +0300

    HUD(Canvas) added

---
+++ Fisholution/HUD.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+signal start_game # emit when we push the start button
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+func game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout") # show message until timeout signal appears (wait time)
+	$StartButton.show()
+	$MessageLabel.text = "Fisholution"
+	$MessageLabel.show()
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
