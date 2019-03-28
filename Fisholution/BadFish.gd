7196195075ff908285252d1bfc544e0b1dc5eef4
nordicebear <nordicebeargames@gmail.com>
2019-03-28 17:32:31 +0300

    adding random scale for bad fishes

--- Fisholution/BadFish.gd
+++ Fisholution/BadFish.gd
@@ -3,6 +3,15 @@ extends RigidBody2D
 export (int) var min_speed
 export (int) var max_speed
 
+func _ready():
+	var rand_scale = rand_range(0.5, 3)
+	var rand_vector = Vector2(rand_scale, rand_scale)
+	$Sprite.scale = rand_vector
+	$CollisionShape2D.scale = rand_vector
+	$CollisionShape2D.rotation_degrees = 90
+
+func _process(delta):
+	$Sprite/AnimationPlayer.play("right")
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free() # if bad fish goes off the screen delete bad fish
