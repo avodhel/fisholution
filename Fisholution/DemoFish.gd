8b9d7ad2c406119eeabdcca982525c2390ec069f
nordicebear <nordicebeargames@gmail.com>
2019-03-30 14:04:50 +0300

    demofish for testing somethings

---
+++ Fisholution/DemoFish.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+export (int) var min_speed
+export (int) var max_speed
+
+var sprite_scale
+
+func _ready():
+	var rand_scale = rand_range(0.5, 3)
+	var rand_vector = Vector2(rand_scale, rand_scale)
+	scale = rand_vector
+#	sprite_scale = $Sprite.scale
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free() # if bad fish goes off the screen delete bad fish
+	
+
