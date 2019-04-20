bff73bf3b0040016cc3644a7d585c330f7b3d8d2
nordicebear <nordicebeargames@gmail.com>
2019-04-20 18:01:21 +0300

    some adjustments in main script and scene

--- Fisholution/Scripts/Main.gd
+++ Fisholution/Scripts/Main.gd
@@ -1,9 +1,9 @@
 extends Node
 
-export (Array, PackedScene) var BadFishes
+export (Array, PackedScene) var Enemies
 
 onready var fish = $Fish
-onready var fish_bfp_bfsl = $Fish/BadFishPath/BFSpawnLocation
+onready var enemy_spawn_location = $Fish/EnemyPath/EnemySpawnLocation
 onready var start_position = $StartPosition
 onready var hud = $HUD
 onready var hud_fb = $HUD/FisholutionBar
@@ -11,7 +11,7 @@ onready var hud_sl = $HUD/ScoreLabel
 onready var hud_hsl = $HUD/HighscoreLabel
 onready var start_timer = $StartTimer
 onready var score_timer =$ScoreTimer
-onready var badfish_timer = $BadFishTimer
+onready var enemy_timer = $EnemyTimer
 onready var gameover_sound = $GameOverSound
 onready var music = $Music
 
@@ -33,14 +33,14 @@ func new_game():
 
 func game_over():
 	score_timer.stop()
-	badfish_timer.stop()
+	enemy_timer.stop()
 	hud.game_over()
 	gameover_sound.play()
 	music.stop()
 	$"/root/PlayerData".save_highscore(score) #save highscore
 
 func _on_StartTimer_timeout():
-	badfish_timer.start()
+	enemy_timer.start()
 	score_timer.start()
 	hud.update_score(score)
 
@@ -48,22 +48,22 @@ func _on_ScoreTimer_timeout():
 	score += 1
 	hud_sl.text = str(score) #score problem fixed with this line
 
-func _on_BadFishTimer_timeout():
+func _on_EnemyTimer_timeout():
 	# Choose a random location on Path2D.
-    fish_bfp_bfsl.set_offset(randi())
-    # Create a BadFish instance and add it to the scene.
-    var badfish = BadFishes[randi() % BadFishes.size()].instance()
-    add_child(badfish)
-    # Set the badfish's direction perpendicular to the path direction.
-    var direction = fish_bfp_bfsl.rotation + PI / 2
-    # Set the badfish's position to a random location.
-    badfish.position = fish_bfp_bfsl.global_position
+    enemy_spawn_location.set_offset(randi())
+    # Create a enemy instance and add it to the scene.
+    var enemy = Enemies[randi() % Enemies.size()].instance()
+    add_child(enemy)
+    # Set the enemy's direction perpendicular to the path direction.
+    var direction = enemy_spawn_location.rotation + PI / 2
+    # Set the enemy's position to a random location.
+    enemy.position = enemy_spawn_location.global_position
     # Add some randomness to the direction.
     direction += rand_range(-PI / 4, PI / 4) # PI/4 means 45 angle
-    badfish.rotation = direction
+    enemy.rotation = direction
     # Set the velocity (speed & direction).
-    badfish.linear_velocity = Vector2(rand_range(badfish.min_speed, badfish.max_speed), 0)
-    badfish.linear_velocity = badfish.linear_velocity.rotated(direction)
+    enemy.linear_velocity = Vector2(rand_range(enemy.min_speed, enemy.max_speed), 0)
+    enemy.linear_velocity = enemy.linear_velocity.rotated(direction)
     # [DemoFish]Set the velocity (speed & direction).
 #    var badfishRB = badfish.find_node("RigidBody2D")
 #    badfishRB.linear_velocity = Vector2(rand_range(badfish.min_speed, badfish.max_speed), 0)
