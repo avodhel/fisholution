d75c934bb4f5eafef53de2afd7e338ec8308324c
nordicebear <nordicebeargames@gmail.com>
2019-04-20 13:44:43 +0300

    added new textures and new sea weeds

--- Fisholution/Scripts/SeaCreature.gd
+++ Fisholution/Scripts/SeaCreature.gd
@@ -4,10 +4,15 @@ onready var sprite = $Sprite
 onready var animation = $Sprite/AnimationPlayer
 
 func _ready():
-	var rand_scale = rand_range(0.6, 3)
-	var rand_vector = Vector2(rand_scale, rand_scale)
-	sprite.scale = rand_vector
-	sprite.modulate = Color(rand_range(0,1), rand_range(0,1), rand_range(0,1)) #random color
+	if is_in_group("common_sea_weed"):
+		var rand_scale = rand_range(0.6, 3)
+		var rand_vector = Vector2(rand_scale, rand_scale)
+		sprite.scale = rand_vector
+		sprite.modulate = Color(rand_range(0,1), rand_range(0,1), rand_range(0,1)) #random color
+	elif is_in_group("rare_sea_weed"):
+		var rand_scale = rand_range(0.6, 1.5)
+		var rand_vector = Vector2(rand_scale, rand_scale)
+		sprite.scale = rand_vector
 
 func _process(delta):
 	animation.play("idle")
