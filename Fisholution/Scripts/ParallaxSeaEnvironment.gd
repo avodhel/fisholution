22c63e20f3c05cab409f76a94eeed16473d00cc1
nordicebear <nordicebeargames@gmail.com>
2019-04-23 16:21:40 +0300

    renamed some scripts and scenes to clarify

--- Fisholution/Scripts/ParallaxSeaEnvironment.gd
+++ Fisholution/Scripts/ParallaxSeaEnvironment.gd
@@ -1,6 +1,6 @@
 extends ParallaxLayer
 
-export (Array, PackedScene) var sea_creatures
+export (Array, PackedScene) var sea_objects
 
 var screensize_x
 var screensize_y
@@ -10,21 +10,21 @@ func _ready():
 	screensize_y = get_viewport_rect().size.y
 	motion_mirroring.x = screensize_x
 	motion_mirroring.y = screensize_y
-	_spawn_sea_creatures()
+	_spawn_sea_objects()
 
-func _spawn_sea_creatures():
+func _spawn_sea_objects():
 	randomize()
 	for counter in range(rand_range(75, 105)):
 		var position_x = rand_range(screensize_x - (screensize_x - 1), screensize_x)
 		var position_y = rand_range(screensize_y - (screensize_y - 1), screensize_y)
-		var sea_creature = sea_creatures[randi() % sea_creatures.size()].instance()
-		if sea_creature.is_in_group("rare_sea_weed"):
+		var sea_object = sea_objects[randi() % sea_objects.size()].instance()
+		if sea_object.is_in_group("rare_sea_weed"):
 			if rand_range(0, 100) > 80:
-				add_child(sea_creature)
+				add_child(sea_object)
 			else:
 				continue
-		elif sea_creature.is_in_group("common_sea_weed"):
-			add_child(sea_creature)
+		elif sea_object.is_in_group("common_sea_weed"):
+			add_child(sea_object)
 		else:
-			add_child(sea_creature)
-		sea_creature.position = Vector2(position_x, position_y)
+			add_child(sea_object)
+		sea_object.position = Vector2(position_x, position_y)
