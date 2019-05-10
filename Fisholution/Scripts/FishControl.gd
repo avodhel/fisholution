04d487922a223da3d863ac3da4190c4271ded0f6
nordicebear <nordicebeargames@gmail.com>
2019-05-11 00:58:28 +0300

    mobile control cleared from hard codes

--- Fisholution/Scripts/FishControl.gd
+++ Fisholution/Scripts/FishControl.gd
@@ -16,7 +16,7 @@ var distance_y
 
 func _ready():
 	screensize = get_viewport_rect().size
-	center = get_viewport_rect().size / 2 #center of the screen
+	center = screensize / 2 #center of the screen
 	up_center = center / Vector2(1, 2)
 	down_center = center + Vector2(0, up_center.y)
 	left_center = center / Vector2(2, 1)
@@ -31,44 +31,44 @@ func _input(event):
 
 func _mobile_control(event):
 	if (event is InputEventMouseButton or event is InputEventScreenTouch) and event.is_pressed():
-		if event.position.x > 120 and event.position.x < 360 and event.position.y < up_center.y : #up
+		if (event.position.x > screensize.x / 4) and (event.position.x < screensize.x * 3 / 4) and (event.position.y < up_center.y): #up
 			_direction("up")
-		if event.position.x > 120 and event.position.x < 360 and event.position.y > down_center.y : #down
+		if (event.position.x > screensize.x / 4) and (event.position.x < screensize.x * 3 / 4) and (event.position.y > down_center.y): #down
 			_direction("down")
-		if event.position.x < left_center.x and  event.position.y > 180 and event.position.y < 540 : # left
+		if (event.position.x < left_center.x) and  (event.position.y > screensize.y / 4) and (event.position.y < screensize.y * 3 / 4): # left
 			_direction("left")
-		if event.position.x > right_center.x and  event.position.y > 180 and event.position.y < 540: # right
+		if (event.position.x > right_center.x) and  (event.position.y > screensize.y / 4) and (event.position.y < screensize.y * 3 / 4): # right
 			_direction("right")
-		if event.position.x < 120 and event.position.y < 180: #up-left
+		if (event.position.x < screensize.x / 4) and (event.position.y < screensize.y / 4): #up-left
 			_direction("up-left")
-		if event.position.x > 360 and event.position.y < 180: #up-right
+		if (event.position.x > screensize.x * 3 / 4) and (event.position.y < screensize.y / 4): #up-right
 			_direction("up-right")
-		if event.position.x < 120 and event.position.y > 540: #down-left
+		if (event.position.x < screensize.x / 4) and (event.position.y > screensize.y * 3 / 4): #down-left
 			_direction("down-left")
-		if event.position.x > 360 and event.position.y > 540: #down-right
+		if (event.position.x > screensize.x * 3 / 4) and (event.position.y > screensize.y * 3 / 4): #down-right
 			_direction("down-right")
-		if event.position.x > 120 and event.position.x < 240 and event.position.y > 180 and event.position.y < 360: #up or left?
+		if (event.position.x > screensize.x / 4) and (event.position.x < screensize.x / 2) and (event.position.y > screensize.y / 4) and (event.position.y < screensize.y / 2): #up or left?
 			distance_y = event.position.y - up_center.y
 			distance_x = event.position.x - left_center.x
 			if distance_x < distance_y: # left
 				_direction("left")
 			else: # up
 				_direction("up")
-		if event.position.x > 240 and event.position.x < 360 and event.position.y > 180 and event.position.y < 360: #up or right?
+		if (event.position.x > screensize.x / 2) and (event.position.x < screensize.x * 3 / 4) and (event.position.y > screensize.y / 4) and (event.position.y < screensize.y / 2): #up or right?
 			distance_y = event.position.y - up_center.y
 			distance_x = right_center.x - event.position.x
 			if distance_x < distance_y: # right
 				_direction("right")
 			else: # up
 				_direction("up")
-		if event.position.x > 120 and event.position.x < 240 and event.position.y > 360 and event.position.y < 540: #down or left?
+		if (event.position.x > screensize.x / 4) and (event.position.x < screensize.x / 2) and (event.position.y > screensize.y / 2) and (event.position.y < screensize.y * 3 / 4): #down or left?
 			distance_y = down_center.y - event.position.y
 			distance_x = event.position.x - left_center.x
 			if distance_x < distance_y: # left
 				_direction("left")
 			else: # down
 				_direction("down")
-		if event.position.x > 240 and event.position.x < 360 and event.position.y > 360 and event.position.y < 540: #down or right?
+		if (event.position.x > screensize.x / 2) and (event.position.x < screensize.x * 3 / 4) and (event.position.y > screensize.y / 2) and (event.position.y < screensize.y * 3 / 4): #down or right?
 			distance_y = down_center.y - event.position.y
 			distance_x = right_center.x - event.position.x
 			if distance_x < distance_y: # right
