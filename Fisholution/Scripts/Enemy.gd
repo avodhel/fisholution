a669aaeb7a5f6543653dd046a0eac3416e7f2be0
nordicebear <nordicebeargames@gmail.com>
2019-05-15 23:59:02 +0300

    same kind fishes can't eat each other  now

--- Fisholution/Scripts/Enemy.gd
+++ Fisholution/Scripts/Enemy.gd
@@ -16,7 +16,16 @@ var velocity = Vector2()
 var direction 
 var speed
 
+var badfish_no
+
 func _ready():
+	_prepare_badfish()
+	_detect_badfish()
+
+func _process(delta):
+	_move(delta)
+
+func _prepare_badfish():
 	randomize()
 	direction = rand_range(-25, 25)
 	self.rotation = direction
@@ -25,9 +34,6 @@ func _ready():
 	scale = rand_vector
 	speed = rand_range(min_speed, max_speed)
 	animation.play("move")
-
-func _process(delta):
-	_move(delta)
 
 func _move(delta):
 	velocity.y -= 1
@@ -38,6 +44,52 @@ func _on_VisibilityNotifier2D_screen_exited():
 	pass
 
 func _on_Enemy_area_entered(area):
-	if area.scale > scale:
-		call_deferred("free")
-		emit_signal("fish_died", self)
+	if self.is_in_group("badfish") and area.is_in_group("badfish"):
+		if self.badfish_no != area.badfish_no: #if badfishes are not same kind
+			if area.scale > scale:
+				call_deferred("free")
+				emit_signal("fish_died", self)
+
+func _detect_badfish():
+	if self.is_in_group("fish1"):
+		badfish_no = 0
+	elif self.is_in_group("fish2"):
+		badfish_no = 1
+	elif self.is_in_group("fish3"):
+		badfish_no = 2
+	elif self.is_in_group("fish4"):
+		badfish_no = 3
+	elif self.is_in_group("fish5"):
+		badfish_no = 4
+	elif self.is_in_group("fish6"):
+		badfish_no = 5
+	elif self.is_in_group("fish7"):
+		badfish_no = 6
+	elif self.is_in_group("fish8"):
+		badfish_no = 7
+	elif self.is_in_group("fish9"):
+		badfish_no = 10
+	elif self.is_in_group("fish10"):
+		badfish_no = 11
+	elif self.is_in_group("fish12"):
+		badfish_no = 12
+	elif self.is_in_group("fish13"):
+		badfish_no = 13
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Fisholution/Scripts/FishControl.gd
+++ Fisholution/Scripts/FishControl.gd
@@ -163,18 +163,25 @@ func _direction(dir):
 			rotation_degrees = 135
 
 func _on_Fish_area_entered(area):
-	print(area)
 	if area.is_in_group("enemy") and area.is_in_group("badfish"): # if enemy is a fish
-		if area.scale >= (scale + Vector2(0.7, 0.7)): #if badfish is bigger than our fish
-			_die() #our fish died
-		elif (area.scale + Vector2(0.5, 0.5)) <= scale: #badfish is smaller than our fish
-			area.hide()
-			area.queue_free()
-			emit_signal("xp_gained")
-#			nom_sound.play()
-		else:
-			print(scale)
-			print(area.scale)
+		#badfish's kind kontrol
+		var same_fish_control = false
+		if Global.fish_no != area.badfish_no:
+			same_fish_control = false
+		elif Global.fish_no == area.badfish_no:
+			same_fish_control = true
+
+		if !same_fish_control: # if badfish is not same kind fish
+			if area.scale >= (scale + Vector2(0.7, 0.7)): #if badfish is bigger than our fish
+				_die() #our fish died
+			elif (area.scale + Vector2(0.5, 0.5)) <= scale: #badfish is smaller than our fish
+				area.hide()
+				area.queue_free()
+				emit_signal("xp_gained")
+	#			nom_sound.play()
+#			else:
+#				print(scale)
+#				print(area.scale)
 	elif area.is_in_group("enemy") and area.is_in_group("not_fish"): #if enemy is not fish, we can't eat it but they can eat us
 		if area.scale >= (scale + Vector2(0.7, 0.7)): #if enemy is bigger than our fish
 			_die() #our fish died
