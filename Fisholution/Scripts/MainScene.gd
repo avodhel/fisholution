d244c60bb963c4897c2a1c3a9cec58f019102428
nordicebear <nordicebeargames@gmail.com>
2019-05-19 15:12:31 +0300

    added mode screen to main scene

--- Fisholution/Scripts/MainScene.gd
+++ Fisholution/Scripts/MainScene.gd
@@ -1,6 +1,8 @@
 extends CanvasLayer
 
-onready var high_score = $HighscoreLabel
+onready var mainscreen_elements = $MainScreenElements
+onready var modes = $Modes
+onready var high_score = $MainScreenElements/HighscoreLabel
 
 var high_score_value
 
@@ -9,4 +11,11 @@ func _ready():
 	high_score.text = "Highscore: " + str(high_score_value)
 
 func _on_StartButton_pressed():
+	mainscreen_elements.hide()
+	modes.show()
+
+func _on_FisholutionStart_pressed():
+	pass # Replace with function body.
+
+func _on_NormalStart_pressed():
 	Global.change_scene("ChooseScene") # open fish choose screen
